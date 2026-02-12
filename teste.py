d89f7ae48482d78c3d6bf8f9e14a40fd889c2cc7
Samuel Brito <samuelbrito599@gmail.com>
2026-02-11 23:17:19 -0300

    Finalizado

--- teste.py
+++ teste.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import os
-import glob
+from glob import glob
 import numpy as np
 
-caminho = r"C:\Users\samue\Desktop\Operação Logística"
+pasta_atual = os.getcwd()
+caminho = os.path.join(pasta_atual, "Automação Relatório Operacional/operacao_logistica")
 
 def converteNumero(df:pd.DataFrame,colunas=[]) -> pd.DataFrame:
     for coluna in colunas:
@@ -13,7 +14,6 @@ def converteNumero(df:pd.DataFrame,colunas=[]) -> pd.DataFrame:
     
     return df
     
-
 def trataPlanilha(nome_arquivo,nome_planilha):
     df = pd.read_excel(nome_arquivo, sheet_name=nome_planilha)
     mes = df.iloc[0,1]
@@ -30,26 +30,41 @@ def trataPlanilha(nome_arquivo,nome_planilha):
 def trataArquivos(arquivo):
     planilhas = []
     for planilha in pd.ExcelFile(arquivo).sheet_names:
-        planilhaTratada = trataPlanilha(arquivo,planilha)
-        planilhas.append(planilhaTratada)
+        try:
+            planilhaTratada = trataPlanilha(arquivo,planilha)
+            planilhas.append(planilhaTratada)
+        except Exception as e:
+            print(f"Erro {type(e).__name__} na planilha {planilha} do arquivo {arquivo}:\n{e}\n")
     df = pd.concat(planilhas, ignore_index=True)
     return df
 
 
-def TrataPastas(camihno):
+def trataPastas(caminho) -> pd.DataFrame:
     pastas = os.listdir(caminho)
     dfs = []
     dfs_arquivos = []
     for i in pastas:
-        arquivos = glob.glob(os.path.join(fr"{caminho}\{i}","*.xlsx"))
+        arquivos = glob(os.path.join(caminho,i,"*.xlsx"))
         
         for arquivo in arquivos:
-            nome_arquivo = arquivo
-            df = trataArquivos(arquivo)
-            df.insert(0,"Estado",nome_arquivo)
-            dfs_arquivos.append(df)
-            
+            try:
+                estado = os.path.basename(arquivo)
+                estado = os.path.splitext(estado)[0].split("_")[-1]
+                df = trataArquivos(arquivo)
+                df.insert(0,"Estado",estado)
+                dfs_arquivos.append(df)
+            except Exception as e:
+                print(f"Erro {type(e).__name__} no arquivo {arquivo}:\n{e}\n")
+
     dfs = pd.concat(dfs_arquivos, ignore_index=True)
+    
+
     return dfs
 
-print(TrataPastas(caminho))
+def main():
+    arquivo_final = trataPastas(caminho)
+    arquivo_final.to_excel("arquivo_final.xlsx", index=False)
+    print(arquivo_final)
+
+if __name__ == "__main__":
+    main()
